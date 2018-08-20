a17b8058d233ac6dfef53d602acb24b7db656362
LiuJianjun <jianjun@dappworks.cn>
2018-08-20 08:44:48 +0800

    增加axios

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 import store from './store'
 
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
